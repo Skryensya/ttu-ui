85f8d10567eaaab1135c416774775d070e5d827c
Allison <Allison.jpb@gmail.com>
2024-11-06 01:50:03 -0300

    fix hero mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,11 +21,13 @@ export function Hero({
   ctaButton = HeroDummyProps.ctaButton,
 }) {
   return (
-    <div className="bg-[url('/test-bg.png')] bg-center bg-no-repeat bg-cover h-[700px] max-h-[80dvh]">
-      <div className="h-full flex flex-col justify-end items-center">
-        <div className="max-w-6xl mx-auto   text-white w-full">
-          <div className="w-5/12 pb-32">
-            <h1 className="font-headings-2 text-5xl leading-[56px]">{title}</h1>
+    <div className="bg-[url('/test-bg.png')] bg-center bg-no-repeat bg-cover h-[700px] max-h-[70dvh] lg:max-h-[80dvh]">
+      <div className="h-full flex flex-col justify-end items-center px-4 ">
+        <div className="max-w-6xl mx-auto text-white w-full">
+          <div className="w-10/12 lg:w-5/12 pb-12 lg:pb-32">
+            <h1 className="font-headings-2 text-3xl lg:text-5xl lg:leading-[56px] whitespace-wrap ">
+              {title}
+            </h1>
             <p className="mt-6">{subtitle}</p>
 
             <Button asChild className="mt-6">
